40c15936514dd068fa814916c0db676bad601154
Niek Bosch <just.niek@gmail.com>
2020-09-06 20:01:00 +0200

    fix: query stale state (#63)

--- src/index.js
+++ src/index.js
@@ -196,7 +196,7 @@ export function ReactQueryDevtools({
 }
 
 const getStatusRank = q =>
-  q.state.isFetching ? 0 : !q.observers.length ? 3 : q.state.isStale ? 2 : 1
+  q.state.isFetching ? 0 : !q.observers.length ? 3 : q.isStale() ? 2 : 1
 
 const sortFns = {
   'Status > Last Updated': (a, b) =>
